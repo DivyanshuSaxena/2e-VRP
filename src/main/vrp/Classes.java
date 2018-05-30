e483281311a1ff5f11cea6a5bd0f8b316644bb20
DivyanshuSaxena <dssaxena2011@gmail.com>
2018-05-30 22:42:47 +0800

    Savings Algorithm in progress

--- src/main/vrp/Classes.java
+++ src/main/vrp/Classes.java
@@ -4,10 +4,10 @@ import java.util.Vector;
 
 // Class for the representation of the customers
 class Customer {
-    // This id is the id of the car park as in the overall nodes (This must be used for getting the distances)    
-    int id;
+    int id; // This id is the id of the customer as in the overall nodes (This must be used for getting the distances).        
     int demand;
-    int assignedPark;
+    int assignedPark; // This is the id of the carpark object that is assigned to this customer.
+    boolean hasCar;
     public void setId(int id) {
         this.id = id;
     }
@@ -17,16 +17,18 @@ class Customer {
 }
 
 class Carpark {
-    // This id is the id of the car park as in the overall nodes (This must be used for getting the distances)
-    int id;
+    int id; // This id is the id of the car park as in the overall nodes (This must be used for getting the distances)
     Vector<Customer> customers;
-    Vector<Vector<Integer>> routes; 
+    Vector<Route> routes; 
     int totalDemand;
     public Carpark() {
         this.id = 0;
         this.totalDemand = 0;
         customers = new Vector<Customer>();
-        routes = new Vector<Vector<Integer>>();
+        routes = new Vector<Route>();
+    }
+    public void setId(int id) {
+        this.id = id;
     }
     public void addCustomer(Customer c) {
         customers.add(c);
@@ -36,10 +38,38 @@ class Carpark {
         customers.remove(c);
         totalDemand = totalDemand - c.demand;
     }
-    public void setId(int id) {
-        this.id = id;
+    public void getInitialRoutes() {
+        // Function to get the initial routes for the current carpark object based on the customers vector
+        // Using Clarke and Wright's Savings Algorithm
+
     }
+}
+
+class Route {
+    Vector<Integer> route;
+    int routeCost;
+    int demand;
+    public Route() {
+        route = new Vector<Integer>();
+        routeCost = 0;
+        demand = 0;
+    }
+    public void addCustomer(int id) {
+        route.add(id);
+        demand += Main.customers[id-Main.numCarpark-1].demand;
+        routeCost += Main.nodesDistance[route.elementAt(route.size()-2)][route.elementAt(route.size()-1)];
+    }
+}
+
+class Solution {
+    Vector<Carpark> firstLevel;
+    Vector<Route> routes;
     public void updateRoutes() {
-        // Function to update the routes of the current object based on the customers vector for the object
+        // Function to update the routes for the first level routes
+    }
+    public int getCost() {
+        // Function to evaluate the total costs of the solution.
+        // Note :- This includes the infeasibility costs
+        return 0;
     }
 }
--- src/main/vrp/Main.java
+++ src/main/vrp/Main.java
@@ -62,11 +62,78 @@ class Main {
             }
         }
 
-        getInitialSoln();
+        Solution initsol = getInitialSoln();
+        // Use initsol to develop the further solutions.
         sc.close();
     }
 
-    public static void getInitialSoln() {
+    public static Solution getInitialSoln() {
         // Finds the initial solution and places it in the static variable route.
+        Solution initial = new Solution();
+        
+        // Assign each customer to the nearest carpark.
+        for(int i = 0; i < numCustomers; i++) {
+            int minDistance = -1;
+            int assigned = 0;
+            for(int j = 1; j <= numCarpark; j++) {
+                if (minDistance > nodesDistance[i][j] || minDistance == -1) {
+                    minDistance = nodesDistance[i][j];
+                    assigned = j;
+                }
+            }
+            customers[i].assignedPark = assigned; // Customer at index i in customers array is assigned the carpark at index 'assigned' in nodes numbering 
+            carparks[assigned-1].addCustomer(customers[i]); // The carpark also holds the customer now.
+        }
+
+        // Now apply Clarke and Wright's Savings Algorithm for the first level
+
+        return initial;
+    }
+
+    public static Vector<Route> savingSolution(Vector<Customer> customers, int depot) {
+        Vector<Route> routes = new Vector<Route>();
+        Vector<Route> list = new Vector<Route>();
+        for(int i = 0; i < customers.size()-1; i++) {
+            Route single = new Route();
+            single.addCustomer(depot);
+            single.addCustomer(customers.elementAt(i).id);
+            single.addCustomer(depot);        
+            routes.add(single); // For adding the single customer routes
+            for(int j = i+1; j < customers.size(); j++) {
+                Route r = new Route();
+                r.addCustomer(depot);
+                r.addCustomer(customers.elementAt(i).id);
+                r.addCustomer(customers.elementAt(j).id);
+                r.addCustomer(depot);
+                list.add(r);
+            }
+        }
+        list.sort(new Comparator<Route>() {
+            @Override
+            public int compare(Route o1, Route o2) {
+                int firsto1 = o1.route.elementAt(1);
+                int firsto2 = o2.route.elementAt(1);
+                int secondo1 = o1.route.elementAt(2);
+                int secondo2 = o2.route.elementAt(2);
+                int savings1 = nodesDistance[depot][firsto1] + nodesDistance[depot][secondo1] - nodesDistance[firsto1][secondo1];
+                int savings2 = nodesDistance[depot][firsto2] + nodesDistance[depot][secondo2] - nodesDistance[firsto2][secondo2];
+                if (savings1 > savings2 ) {
+                    return 1;
+                } else if (savings1 == savings2 && o1.demand < o2.demand) {
+                    return 1;
+                }
+                return -1;
+            }
+        });
+        // Now we have the sorted list, arranged in descending order as per the savings
+        for(int i = 0; i < list.size(); i++) {
+            Route bestSavings = list.elementAt(i); // This is the yet best merge for two delivery locations
+            for(int j = 0; j < routes.size(); j++) {
+                Route currRoute = routes.elementAt(j);
+                
+                if(currRoute.route.firstElement() == bestSavings.route.firstElement())
+            }
+        } 
+        return routes;
     }
 }
