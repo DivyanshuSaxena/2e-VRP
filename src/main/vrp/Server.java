b31588dc8b80aaf985f5acead9bd25b32e82b72a
DivyanshuSaxena <dssaxena2011@gmail.com>
2018-07-13 21:32:47 +0800

    Server class added

---
+++ src/main/vrp/Server.java
@@ -0,0 +1,51 @@
+package vrp;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class Server {
+    ServerSocket ss;
+    Socket socket;
+    DataInputStream in;
+    public Server(int port) {
+        try
+        {
+            ss = new ServerSocket(port);
+            System.out.println("Server started");
+            System.out.println("Waiting for a client ...");
+ 
+            socket = ss.accept();
+            System.out.println("Client accepted");
+ 
+            // takes input from the client socket
+            in = new DataInputStream(
+                new BufferedInputStream(socket.getInputStream()));
+ 
+            String line = "";
+ 
+            // reads message from client until "Over" is sent
+            while (!line.equals("EOF"))
+            {
+                try
+                {
+                    line = in.readUTF();
+                    System.out.println(line);
+ 
+                }
+                catch(IOException i)
+                {
+                    System.out.println(i);
+                }
+            }
+            System.out.println("Closing connection");
+ 
+            // close connection
+            socket.close();
+            in.close();
+        }
+        catch(IOException i)
+        {
+            System.out.println(i);
+        }
+    }
+}
